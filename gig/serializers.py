67f077ba6b29a83dc252f4b9bb38401a4003fa65
Sayed Rafi <kb-md.sayed.ar.rafi@keex.co.jp>
2022-11-30 17:05:44 +0900

    Update serializers.py

--- gig/serializers.py
+++ gig/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import Gig
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "username", "avatar"]
 
 
 class GigSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+    num_vote_up = serializers.ReadOnlyField()
+    num_vote_down = serializers.ReadOnlyField()
+    
     class Meta:
         model = Gig
         fields = [
@@ -18,7 +31,9 @@ class GigSerializer(serializers.ModelSerializer):
             "category",
             "upvotes",
             "downvotes",
+            "num_vote_up",
+            "num_vote_down",
             "created_at",
-            "updated_at"
+            "updated_at",
         ]
         read_only_fields = ["author"]
