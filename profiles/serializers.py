0f2089bf1a89b7d4317a51c7c244630cdb1fdae2
Sayed Ar Rafi <51741328+SAR-dev@users.noreply.github.com>
2022-12-18 12:00:14 +0900

    Update serializers.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
 from .models import Profiles
+from comment.models import Comment
+from django.db.models import Avg
+from django.contrib.auth import get_user_model
+User = get_user_model()
+   
+class UserProfileSerializer(serializers.ModelSerializer):
+    total_ratings = serializers.SerializerMethodField()
+    average_rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "username", "avatar", "total_ratings", "average_rating", "date_joined"]
+
+    def get_total_ratings(self, obj):
+        return Comment.ratings.filter(gig__author_id = obj.id).count()
+    
+    def get_average_rating(self, obj):
+        return Comment.ratings.filter(gig__author_id = obj.id).aggregate(Avg("rating"))
+    
 
 class ProfilesWriteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,6 +48,7 @@ class ProfilesWriteSerializer(serializers.ModelSerializer):
 
 class ProfilesReadSerializer(serializers.ModelSerializer):
     owner = serializers.SerializerMethodField()
+    author = UserProfileSerializer(read_only=True)
 
     class Meta:
         model = Profiles
@@ -58,4 +78,4 @@ class ProfilesReadSerializer(serializers.ModelSerializer):
 
     def get_owner(self, obj):
         return obj.author == self.context.get('request').user
-    
+ 
