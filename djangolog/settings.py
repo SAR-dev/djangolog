f8524945b5d9da419ecf133c4fd79fac37452e17
Sayed Rafi <kb-md.sayed.ar.rafi@keex.co.jp>
2023-01-15 19:01:38 +0900

    Update settings.py

--- djangolog/settings.py
+++ djangolog/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
+    "cloudinary",
     "rest_framework",
     "rest_framework.authtoken",
     "django_filters",
